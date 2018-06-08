f058754e8dbc4824733422441d770ef39ef7fbc4
zerino <leo.zerino@gmail.com>
2018-06-08 02:30:07 -0300

    conteudo display: none para subir no servidor

---
+++ resources/views/home1.blade.php
@@ -0,0 +1,30 @@
+<!doctype html>
+<html lang="{{ app()->getLocale() }}">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="{{asset('css/app.css')}}"> 
+        <script src="{{asset('js/jquery.min.js')}}"></script>
+        <script src="{{asset('js/app.min.js')}}"></script>
+        <title>▲</title>
+        <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+    </head>
+    <body>
+        <div class="flex-center position-ref full-height">
+            <div class="content">
+                <div class="title m-b-md">
+                    transaKryTica
+                </div>
+
+                <div class="links">
+                    <a href="#">sobre</a>
+                    <a href="#">lowcura</a>
+                    <a href="#">flerte</a>
+                    <a href="#">gentes</a>
+                    <a href="#">mídia</a>
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
